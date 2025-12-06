55c9d7295170b8c5f704a9c38187b3efc722dab1
Toufiqul2022 <toufiqul8865@gmail.com>
2025-12-06 14:01:42 +0600

    commit

--- src/Pages/AddServices.jsx
+++ src/Pages/AddServices.jsx
@@ -52,7 +52,7 @@ const AddServices = () => {
   };
 
   return (
-    <div className="max-w-xl mx-auto p-6 bg-white shadow-lg rounded-xl my-10">
+    <div className="max-w-xl mx-auto p-6 shadow-lg rounded-xl my-10">
       <h2 className="text-2xl font-bold mb-5 text-gray-700">
         Add Product / Pet
       </h2>
